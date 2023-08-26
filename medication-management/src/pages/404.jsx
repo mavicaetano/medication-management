ffa9bdbbfc7e428f2256fcf6c937c03958cc50b2
Maria Vicente <mnvcaetano@gmail.com>
2023-08-26 18:48:19 -0300

    feat: adiciona rotas às páginas

---
+++ medication-management/src/pages/404.jsx
@@ -0,0 +1,7 @@
+function NotFound() {
+    return (
+        <h1>Erro 404: página não encontrada</h1>
+    )
+}
+
+export { NotFound }
---
+++ medication-management/src/routes/index.jsx
@@ -0,0 +1,6 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Login } from '../pages/login';
+import { Map } from '../pages/map-page';
+import { MedicineList } from '../pages/medicine-list';
+import { NewMedicine } from '../pages/new-medicine';
+import { NewPharmacy } from '../pages/new-pharmacy';
