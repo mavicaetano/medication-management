39657e5d122ba661acbd205e875b6072f948db51
Maria Vicente <mnvcaetano@gmail.com>
2023-08-31 21:05:37 -0300

    feat: importa a biblioteca bootstrap e o arquivo styles.css

--- medication-management/src/main.jsx
+++ medication-management/src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "./styles.css";
 import App from './App.jsx'
+import { RoutesApp } from './routes/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <RoutesApp />
   </React.StrictMode>,
 )
