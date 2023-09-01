603f1aa438d0af1dfacc0757ccb5984ef737540c
Maria Vicente <mnvcaetano@gmail.com>
2023-08-31 21:04:35 -0300

    feat: adiciona estilos para o menu

---
+++ medication-management/src/components/menu.jsx
@@ -0,0 +1,55 @@
+import { Link } from "react-router-dom";
+
+function Menu() {
+    const pages = [
+        {
+            route: "/home",
+            description: "Mapa",
+        },
+        {
+            route: "/medicine-list",
+            description: "Lista de medicamentos",
+        },
+        {
+            route: "/new-medicine",
+            description: "Cadastrar medicamento",
+        },
+        {
+            route: "/new-pharmacy",
+            description: "Cadastrar farmácia",
+        }
+    ];
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav flex-column">
+                        {pages.map(({ route, description }) => {
+                            return (
+                                <li className="nav-item" key={description}>
+                                    <Link className="nav-link" to={route}>
+                                        {description}
+                                    </Link>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export { Menu };
